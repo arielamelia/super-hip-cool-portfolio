0ccd743ea0071153aac88db26abfd276ea080fdb
Ariel <arielamelia@knights.ucf.edu>
2017-02-23 08:51:16 -0500

    Moved JQuery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,6 @@
 console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+	$('#demosMenu').change(function(){
+	  window.location.href = $(this).find('option:selected').attr('id') + '.html';
+	});
+});
