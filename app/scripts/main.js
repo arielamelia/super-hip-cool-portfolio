9b7f2dc4ec7bc3a0f04df528f4b8003187d1833c
Ariel <arielamelia@knights.ucf.edu>
2017-02-27 00:22:19 -0500

    Carousel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,4 +3,5 @@ $(document).ready(function(){
 	$('#demosMenu').change(function(){
 	  window.location.href = $(this).find('option:selected').attr('id') + '.html';
 	});
+	 $('.carousel.carousel-slider').carousel({fullWidth: true});
 });
