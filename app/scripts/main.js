61ca5a0242747d85cad4dd81f92048576a165943
Ariel <arielamelia@knights.ucf.edu>
2017-03-01 14:11:03 -0500

    Carousel added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,51 @@
 console.log('\'Allo \'Allo!');
 $(document).ready(function(){
+ $('.carousel').carousel();
+
 	$('#demosMenu').change(function(){
 	  window.location.href = $(this).find('option:selected').attr('id') + '.html';
 	});
-	 $('.carousel.carousel-slider').carousel({fullWidth: true});
+	   $('#demosMenu').change(function(){
+            window.location.href = $(this).find('option:selected').attr('id') + '.html';
+          });
+          var deleteLog = false;
+          $('#fullpage').fullpage({
+            sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+            anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+            menu: '#menu',
+            onLeave: function(index, nextIndex, direction){
+              if(deleteLog){
+                $('#callbacksDiv').html('');
+              }
+              $('#callbacksDiv').append('<p>onLeave - index:' + index + ' nextIndex:' + nextIndex + ' direction:' + direction + '</p>')
+              console.log("onLeave--" + "index: " + index + " nextIndex: " + nextIndex + " direction: " +  direction);
+            },
+            onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){
+              if(deleteLog){
+                $('#callbacksDiv').html('');
+              }
+              $('#callbacksDiv').append('<p>onSlideLeave - anchorLink:' + anchorLink + " index:" + index + " slideIndex:" + slideIndex +" direction:" + direction + " nextSlideIndex:" + nextSlideIndex + '</p>');
+              console.log("onSlideLeave--" + "anchorLink: " + anchorLink + " index: " + index + " slideIndex: " + slideIndex + " direction: " + direction);
+            },
+            afterRender: function(){
+              $('#callbacksDiv').append('<p>afterRender</p>');
+              console.log("afterRender");
+            },
+            afterResize: function(){
+              $('#callbacksDiv').append('<p>afterResize</p>');
+              console.log("afterResize");
+            },
+            afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
+              $('#callbacksDiv').append('<p>afterSlideLoad - anchorLink:' + anchorLink + " index:" + index + " slideAnchor:" + slideAnchor +" slideIndex:" + slideIndex + '</p>');
+              deleteLog = true;
+              console.log("afterSlideLoad--" + "anchorLink: " + anchorLink + " index: " + index + " slideAnchor: " + slideAnchor + " slideIndex: " + slideIndex);
+              console.log("----------------");
+            },
+            afterLoad: function(anchorLink, index){
+              $('#callbacksDiv').append('<p>afterLoad - anchorLink:' + anchorLink + " index:" + index + '</p>');
+              deleteLog = true;
+              console.log('===============');
+              console.log("afterLoad--" + "anchorLink: " + anchorLink + " index: " + index );
+            }
+          });
 });
