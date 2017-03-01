adbb57edc22ec9e57b54a41111d0da3f52bfbb68
Ariel <arielamelia@knights.ucf.edu>
2017-03-01 14:43:33 -0500

    Added in textillate and slick-carousel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,34 +18,34 @@ $(document).ready(function(){
                 $('#callbacksDiv').html('');
               }
               $('#callbacksDiv').append('<p>onLeave - index:' + index + ' nextIndex:' + nextIndex + ' direction:' + direction + '</p>')
-              console.log("onLeave--" + "index: " + index + " nextIndex: " + nextIndex + " direction: " +  direction);
+              console.log('onLeave--' + 'index: ' + index + ' nextIndex: ' + nextIndex + ' direction: ' +  direction);
             },
             onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){
               if(deleteLog){
                 $('#callbacksDiv').html('');
               }
-              $('#callbacksDiv').append('<p>onSlideLeave - anchorLink:' + anchorLink + " index:" + index + " slideIndex:" + slideIndex +" direction:" + direction + " nextSlideIndex:" + nextSlideIndex + '</p>');
-              console.log("onSlideLeave--" + "anchorLink: " + anchorLink + " index: " + index + " slideIndex: " + slideIndex + " direction: " + direction);
+              $('#callbacksDiv').append('<p>onSlideLeave - anchorLink:' + anchorLink + ' index:' + index + ' slideIndex:' + slideIndex +' direction:' + direction + ' nextSlideIndex:' + nextSlideIndex + '</p>');
+              console.log('onSlideLeave--' + 'anchorLink: ' + anchorLink + ' index: ' + index + ' slideIndex: ' + slideIndex + ' direction: ' + direction);
             },
             afterRender: function(){
               $('#callbacksDiv').append('<p>afterRender</p>');
-              console.log("afterRender");
+              console.log('afterRender');
             },
             afterResize: function(){
               $('#callbacksDiv').append('<p>afterResize</p>');
-              console.log("afterResize");
+              console.log('afterResize');
             },
             afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
-              $('#callbacksDiv').append('<p>afterSlideLoad - anchorLink:' + anchorLink + " index:" + index + " slideAnchor:" + slideAnchor +" slideIndex:" + slideIndex + '</p>');
+              $('#callbacksDiv').append('<p>afterSlideLoad - anchorLink:' + anchorLink + ' index:' + index + ' slideAnchor:' + slideAnchor +' slideIndex:' + slideIndex + '</p>');
               deleteLog = true;
-              console.log("afterSlideLoad--" + "anchorLink: " + anchorLink + " index: " + index + " slideAnchor: " + slideAnchor + " slideIndex: " + slideIndex);
-              console.log("----------------");
+              console.log('afterSlideLoad--' + 'anchorLink: ' + anchorLink + ' index: ' + index + ' slideAnchor: ' + slideAnchor + ' slideIndex: ' + slideIndex);
+              console.log('----------------');
             },
             afterLoad: function(anchorLink, index){
-              $('#callbacksDiv').append('<p>afterLoad - anchorLink:' + anchorLink + " index:" + index + '</p>');
+              $('#callbacksDiv').append('<p>afterLoad - anchorLink:' + anchorLink + ' index:' + index + '</p>');
               deleteLog = true;
               console.log('===============');
-              console.log("afterLoad--" + "anchorLink: " + anchorLink + " index: " + index );
+              console.log('afterLoad--' + 'anchorLink: ' + anchorLink + ' index: ' + index );
             }
           });
 });
